f2b35d6365f4a4ff1b2f76062ec958f2a45ed1c3
lazjo370 <lazarjoveljic@gmail.com>
2024-09-04 16:35:12 +0200

    chore: initialization project and dependencies

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-view>Recipe Board</router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'App',
+});
+</script>
+
+<style>
+/* You can add global styles here */
+</style>
