b9668c4d49f60711de0abe538f3f581ede68d76e
lazjo370 <lazarjoveljic@gmail.com>
2024-09-06 11:21:02 +0200

    feat: add models and database relation, migration

---
+++ database/migrations/2024_09_06_090835_create_recipes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('instruction');
+            $table->string('image')->nullable();
+            $table->enum('cuisine_type', ['English', 'French', 'Italian']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
